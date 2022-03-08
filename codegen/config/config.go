fa70eaf107d6324f5e04be76db25c9054cbf65f4
roneli <38083777+roneli@users.noreply.github.com>
2022-03-08 15:26:28 +0200

    initial commit

--- codegen/config/config.go
+++ codegen/config/config.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cloudquery/cq-gen/code"
 	"github.com/creasty/defaults"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type Config struct {
@@ -114,6 +115,8 @@ type FunctionConfig struct {
 	Generate bool `hcl:"generate,optional"`
 	// PathResolver defines this function to be called the FieldPath traversed, this is used by generic functions
 	PathResolver bool `hcl:"path_resolver,optional"`
+	// Parameters to pass
+	Params []cty.Value `hcl:"params,optional"`
 }
 
 func (r ResourceConfig) GetRelationConfig(name string) *RelationConfig {
